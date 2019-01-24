a076a018123eb7920307d6419ea9b2e1db4b0cd3
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-01-24 15:19:37 -0600

    UI improvements

--- app.js
+++ app.js
@@ -13,9 +13,14 @@ let scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0 ;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 //ADD EVENT LISTENER
 document.querySelector('.btn-roll').addEventListener('click', function(){
@@ -29,7 +34,26 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
    diceDOM.src = 'dice-' + dice + '.png';
 
   // 3. Update the round score only IF called number is not a 1
+  if (dice !== 1) {
+    //Add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    //Next PLayer
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
 
+    //Setting Scores to 0 in interface
+     document.getElementById('current-0').textContent = '0';
+     document.getElementById('current-1').textContent = '0';
+      
+     //Toggling Active Player
+     document.querySelector('.player-0-panel').classList.toggle('active');
+     document.querySelector('.player-1-panel').classList.toggle('active');
+     
+     //Not Displaying Dice when player changes
+     document.querySelector('.dice').style.display = 'none'
+   }
 });
 
 
