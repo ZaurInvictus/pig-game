eb8a763afb6830bf371a41d85a6fb236d2911f1e
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-01-25 12:22:17 -0600

    functionality update"

--- app.js
+++ app.js
@@ -11,19 +11,10 @@ GAME RULES:
 
 let scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0 ;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 //ADD EVENT LISTENER
-document.querySelector('.btn-roll').addEventListener('click', function(){
+document.querySelector('.btn-roll').addEventListener('click', function() {
 
   // 1. Random number
   let dice = Math.floor(Math.random() * 6) + 1;
@@ -40,24 +31,71 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
     //Next PLayer
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
+    nextPlayer();
+   }  
 
-    //Setting Scores to 0 in interface
-     document.getElementById('current-0').textContent = '0';
-     document.getElementById('current-1').textContent = '0';
-      
-     //Toggling Active Player
-     document.querySelector('.player-0-panel').classList.toggle('active');
-     document.querySelector('.player-1-panel').classList.toggle('active');
-     
-     //Not Displaying Dice when player changes
-     document.querySelector('.dice').style.display = 'none'
-   }
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function(){
+//Add CURRENT score to GLOBAL score
+//scores[activePlayer] = scores[activePlayer] + roundScore;
+scores[activePlayer] += roundScore;
+
+//Update the UI
+document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+//Check if player won the game
+if (scores[activePlayer] >= 10) {
+ document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+ document.querySelector('.dice').style.display = 'none';
+ document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+ document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+} else {
+ //Next PLayer
+ nextPlayer();
+}
+
+});
+
+function nextPlayer() {
+   //Next Player
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  //Setting Scores to 0 in interface
+   document.getElementById('current-0').textContent = '0';
+   document.getElementById('current-1').textContent = '0';
+    
+   //Toggling Active Player
+   document.querySelector('.player-0-panel').classList.toggle('active');
+   document.querySelector('.player-1-panel').classList.toggle('active');
+   
+   //Not Displaying Dice when player changes 
+   document.querySelector('.dice').style.display = 'none'
+}
 
 
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+  scores = [0, 0];
+  activePlayer = 0;
+  roundScore = 0;
+
+  document.querySelector('.dice').style.display = 'none';
+
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+}
 
 
 
@@ -66,3 +104,4 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 //To read values & store it in variables
 //let x = document.querySelector('#score-0').textContent;
+
